d3edb8e1ee026a934adfb2375290e596c84cfa83
Maxime R <maxime.rault@digital-links.fr>
2025-08-21 15:37:51 +0200

    [database] create Capture

---
+++ migrations/Version20250821133449.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250821133449 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE TABLE capture (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE TABLE capture_capture_element (capture_id INT NOT NULL, capture_element_id INT NOT NULL, INDEX IDX_504F58326B301384 (capture_id), INDEX IDX_504F5832DE152EAB (capture_element_id), PRIMARY KEY(capture_id, capture_element_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE capture_capture_element ADD CONSTRAINT FK_504F58326B301384 FOREIGN KEY (capture_id) REFERENCES capture (id) ON DELETE CASCADE
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE capture_capture_element ADD CONSTRAINT FK_504F5832DE152EAB FOREIGN KEY (capture_element_id) REFERENCES capture_element (id) ON DELETE CASCADE
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE capture_capture_element DROP FOREIGN KEY FK_504F58326B301384
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE capture_capture_element DROP FOREIGN KEY FK_504F5832DE152EAB
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE capture
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE capture_capture_element
+        SQL);
+    }
+}
---
+++ src/Entity/Capture.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CaptureRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CaptureRepository::class)]
+class Capture
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    /**
+     * @var Collection<int, CaptureElement>
+     */
+    #[ORM\ManyToMany(targetEntity: CaptureElement::class)]
+    private Collection $captureElements;
+
+    public function __construct()
+    {
+        $this->captureElements = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, CaptureElement>
+     */
+    public function getCaptureElements(): Collection
+    {
+        return $this->captureElements;
+    }
+
+    public function addCaptureElement(CaptureElement $captureElement): static
+    {
+        if (!$this->captureElements->contains($captureElement)) {
+            $this->captureElements->add($captureElement);
+        }
+
+        return $this;
+    }
+
+    public function removeCaptureElement(CaptureElement $captureElement): static
+    {
+        $this->captureElements->removeElement($captureElement);
+
+        return $this;
+    }
+}
