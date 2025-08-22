caf8c46e2e0a40417783dcd152cdfd34b263fe33
Maxime R <maxime.rault@digital-links.fr>
2025-08-22 13:20:26 +0200

    Participant role CRUD

--- src/Form/ParticipantRoleForm.php
+++ src/Form/ParticipantRoleForm.php
@@ -2,19 +2,17 @@
 
 namespace App\Form;
 
-use App\Entity\Capture;
 use App\Entity\CaptureElement;
-use App\Form\CaptureElement\CaptureElementForm;
-use App\Form\Field\ConfigFieldForm;
+use App\Entity\ParticipantRole;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class CaptureForm extends AbstractType
+class ParticipantRoleForm extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
@@ -23,7 +21,7 @@ class CaptureForm extends AbstractType
                 'label' => 'Nom',
                 'attr' => [
                     'class' => 'form-control',
-                    'placeholder' => 'Nom de l\'élémént...',
+                    'placeholder' => 'Nom du rôle...',
                 ],
                 'required' => true,
             ])
@@ -33,25 +31,12 @@ class CaptureForm extends AbstractType
                 'attr' => [
                     'class' => 'form-control',
                     'rows' => 3,
-                    'placeholder' => 'Description de la capture...',
+                    'placeholder' => 'Description du rôle...',
                 ],
             ])
-            ->add('captureElements', EntityType::class, [
-                'class' => CaptureElement::class,
-                'choice_label' => 'id',
-                'multiple' => true,
-            ])
-            ->add('captureElements', CollectionType::class, [
-                'entry_type' => CaptureElementForm::class,
-                'allow_add' => true,
-                'allow_delete' => true,
-                'label' => false,
-                'by_reference' => false,
-                'prototype' => true,
-                'entry_options' => ['label' => false],
-                'attr' => [
-                    'data-controller'=> 'capture'
-                ]
+            ->add('internal', CheckboxType::class, [
+                'label'    => 'Rôle interne ?',
+                'required' => false,
             ])
         ;
     }
@@ -59,7 +44,7 @@ class CaptureForm extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Capture::class,
+            'data_class' => ParticipantRole::class,
         ]);
     }
 }
