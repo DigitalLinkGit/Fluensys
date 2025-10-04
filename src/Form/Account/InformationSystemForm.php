c522776704603867e07bbf273d2d796d7a3ed443
Maxime R <maxime.rault@digital-links.fr>
2025-10-04 14:48:52 +0200

    [FORM] SystemComponents

--- src/Form/Account/InformationSystemForm.php
+++ src/Form/Account/InformationSystemForm.php
@@ -2,10 +2,12 @@
 
 namespace App\Form\Account;
 
+use App\Entity\InformationSystem;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
 class InformationSystemForm extends AbstractType
 {
@@ -21,6 +23,21 @@ class InformationSystemForm extends AbstractType
                 ],
                 'required' => true,
             ])
+            ->add('systemComponents', CollectionType::class, [
+                'entry_type' => SystemComponentForm::class,
+                'label' => false,
+                'allow_add' => true,
+                'allow_delete' => true,
+                'by_reference' => false,
+                'prototype' => true,
+                'attr' => [
+                    'class' => 'system-components-collection',
+                ],
+                'entry_options' => [
+                    'label' => false,
+                    'attr' => ['class' => 'row']
+                ],
+            ])
         ;
 
 
@@ -30,7 +47,7 @@ class InformationSystemForm extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => \App\Entity\InformationSystem::class,
+            'data_class' => InformationSystem::class,
         ]);
     }
 }
---
+++ src/Form/Account/SystemComponentForm.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form\Account;
+
+use App\Entity\SystemComponent;
+use App\Enum\SystemComponentType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SystemComponentForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom du composant',
+                'required' => true,
+                'row_attr' => ['class' => 'col-md-4'],
+            ])
+            ->add('type', ChoiceType::class, [
+                'label' => 'Type de composant',
+                'choices' => array_combine(
+                    array_map(fn($c) => ucfirst($c->name), SystemComponentType::cases()),
+                    SystemComponentType::cases()
+                ),
+                'choice_label' => fn(SystemComponentType $choice) => ucfirst($choice->value),
+                'required' => true,
+                'row_attr' => ['class' => 'col-md-4'],
+            ]);
+        ;
+
+
+
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => SystemComponent::class,
+        ]);
+    }
+}
